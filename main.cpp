9bea8a9846e778aa3349a976121bf2f808b0654a
William Bae <williambae1@gmail.com>
2017-10-04 14:46:15 +0900

    Print format change, added MSE calculation for test set

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 #include "MNIST_bin.h"
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
 
-#define WHOLE_DATA_PER_EPOCH
+//#define WHOLE_DATA_PER_EPOCH
 
 int main() {
 	srand(time(NULL));
@@ -47,59 +49,72 @@ int main() {
 		.build();
 
 
-	const int total_size = train_set.size();
-
-	int epoch = 1;
+	int epoch = 154000;
 	double mse = 1e100;
 #ifdef WHOLE_DATA_PER_EPOCH
-	for(int start = epoch; ; epoch++) {
+	const int batch_size = train_set.size();
+	for(int start = ++epoch; ; epoch++) {
 #else
-	const int batch_size = total_size / 50;
-	int batch_start = 0;
-	for (int start = epoch; ; epoch++) {
+	const int batch_size = 100;
+	for (int start = ++epoch; ; epoch++) {
 #endif
 
 		std::random_shuffle(train_set.begin(), train_set.end());
-		std::cout << "Training epoch #" << epoch << "..." << std::endl;
-		int begin = time(NULL);
 
-#ifdef WHOLE_DATA_PER_EPOCH
-		network->train(total_size, &train_set[0], 0.003);
-#else
-		network->train(batch_size, &train_set[batch_start]);
-		batch_start = (batch_start + batch_size) % total_size;
+		// start position doesn't matter; train_set is randomly shuffled every epoch.
+		network->train(batch_size, &train_set[0]);
+
+#ifndef WHOLE_DATA_PER_EPOCH
+		if (epoch % 100 == 0) {
+#endif
+			std::cout << "Epoch #" << epoch <<  " finished, MSE: ";
+
+			double sq_error = 0;
+			int error_count = 0;
+			int correct_num = 0;
+			for (auto iter = test_set.begin(); iter != test_set.end(); ++iter) {
+				auto result = network->predict(iter->data);
+				double lmax = 0, rmax = 0;
+				double li = -1, ri = -1;
+				for (int i = 0; i < dataset.OUTPUTS; i++) {
+					if (iter->label[i] > lmax) {
+						lmax = iter->label[i];
+						li = i;
+					}
+					if(result[i] > rmax) {
+						rmax = result[i];
+						ri = i;
+					}
+
+					double error = result[i] - iter->label[i];
+					sq_error += error * error;
+					error_count++;
+				}
+				if(li == ri) correct_num++;
+			}
+			mse = sq_error / error_count;
+			std::cout << mse << ", Accuracy: " << correct_num * 100.0 / test_set.size() << '%' << std::endl;
+#ifndef WHOLE_DATA_PER_EPOCH
+		}
 #endif
 
-		int end = time(NULL);
-		std::cout << "Finished in " << end - begin << "s, calculating error..." << std::endl;
-
-		double sq_error = 0;
-		int error_count = 0;
-		for (auto iter = train_set.begin(); iter != train_set.end(); ++iter) {
-			double* result = network->predict(iter->data);
-			for (int i = 0; i < dataset.OUTPUTS; i++) {
-				double error = result[i] - iter->label[i];
-				sq_error += error * error;
-				error_count++;
-			}
-		}
-		mse = sq_error / error_count;
-		std::cout << "Done, MSE:" << mse << std::endl;
-
+#ifdef WHOLE_DATA_PER_EPOCH
 		if (epoch % 5 == 0) {
+#else
+		if (epoch % 2000 == 0) {
+#endif
 			char ckptfile[100];
 			sprintf(ckptfile, "./ckpt/%d.ckpt", epoch);
 
-			std::cout << "[Checkpoint reached] Saving to \"" << ckptfile << "\"..." << std::endl;
+			std::cout << std::endl << "[Checkpoint reached] Saving to \"" << ckptfile << "\"..." << std::endl;
 
 			std::ofstream ckpt(ckptfile, std::ios::binary);
 			network->dump_network(ckpt);
 			ckpt.flush();
 			ckpt.close();
 
-			std::cout << "Save complete." << std::endl;
+			std::cout << "Save complete." << std::endl << std::endl;
 		}
-		std::cout << std::endl;
 
 		if (mse < 0.001) {
 			std::cout << "MSE reached the threshold, run more epoches?(Y/n) ";
@@ -115,10 +130,10 @@ int main() {
 	int count = 0;
 	int correct = 0;
 	for (auto iter = test_set.begin(); iter != test_set.end(); ++iter) {
-		double* data = iter->data;
-		double* label = iter->label;
+		auto data = iter->data;
+		auto label = iter->label;
 
-		double* output = network->predict(data);
+		auto output = network->predict(data);
 
 		double max_ans = 0, max_res = 0;
 		int i_ans = 0, i_res = 0;
